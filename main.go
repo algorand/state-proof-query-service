ff48325be7583ef825e0a314514027f92a1ab557
Almog Tal <almog.tal@algorand.com>
2022-08-15 16:17:16 +0300

    Added TODOs, can now handle 404 not found

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
-
+	
 	"github.com/almog-t/state-proof-query-service/querier"
 	"github.com/almog-t/state-proof-query-service/servicestate"
 	"github.com/almog-t/state-proof-query-service/writer"
@@ -16,6 +17,7 @@ func fetchStateProof(state *servicestate.ServiceState, nodeQuerier querier.Queri
 	}
 
 	nextStateProofData, err := nodeQuerier.QueryNextStateProofData(state)
+
 	if err != nil {
 		return err
 	}
@@ -33,6 +35,9 @@ func fetchStateProof(state *servicestate.ServiceState, nodeQuerier querier.Queri
 	return nil
 }
 
+// TODO: Logging
+// TODO: What if the latest block is not in the network?
+// TODO: Separate file for bucket and key
 func main() {
 	state, err := servicestate.InitializeState("state.txt")
 	if err != nil {
@@ -43,16 +48,23 @@ func main() {
 	nodeQuerier, err := querier.InitializeQuerier("/Users/almog/go/src/light-client-demo/demo_instance/demo_network/2")
 	if err != nil {
 		fmt.Printf("Could not initialize querier: %s\n", err)
+		return
 	}
 
-	s3Writer := writer.InitializeWriter("bucket", "key")
+	s3Writer := writer.InitializeWriter(BUCKET, ACCESS_KEY)
 
 	for {
-		time.Sleep(500 * time.Millisecond)
 		err = fetchStateProof(state, *nodeQuerier, *s3Writer)
-		if err != nil {
-			fmt.Printf("Error while fetching state proof: %s\n", err)
-			return
+		if err == nil {
+			continue
 		}
+
+		if strings.Contains(err.Error(), "HTTP 404") {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		}
+
+		fmt.Printf("Error while fetching state proof: %s\n", err)
+		break
 	}
 }
